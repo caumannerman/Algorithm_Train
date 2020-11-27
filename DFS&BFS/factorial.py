86f1e01323f398009b1269ca6b736328b8ef28a4
Caumannerman <uigohiiming2@gmail.com>
2020-11-28 01:14:00 +0900

    Add Permutation, Combination func

--- DFS&BFS/factorial.py
+++ DFS&BFS/factorial.py
@@ -11,3 +11,19 @@ def factorial_recursive(n):
         return 1
     return n* factorial_recursive(n-1)
 
+
+def Pmt(n,r):
+    result = 1
+    for i in range(n, n-r, -1):
+        result *= i
+    return result
+
+
+
+def Cbn(n, r):  # ?? 이게 무조건 정수가 된다는 걸 증명할 수 있으신  분
+
+    result = Pmt(n, r)
+
+    for i in range(r, 1, -1):
+        result //= i
+    return result
