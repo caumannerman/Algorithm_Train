41af09793582988c96a0a09f161152c9d8589fdf
Caumannerman <uigohiiming2@gmail.com>
2020-12-03 19:40:53 +0900

    Disjoint_Set Function SC

---
+++ 그래프 이론/Disjoint_Sets.py
@@ -0,0 +1,46 @@
+'''서로소 집합 ( Disjoint Sets )은 공통 원소가 없는 두 집합을 의미
+
+"서로소 집합" 자료구조는 몇몇 그래프 알고리즘에서 매우 중요
+
+  # 주요 연산 - find,  union    2가지!!
+      find - 특정 원소가 속한 집합이 어떤 집합인지 알려주는 연산
+      union - 합집합 연산으로, 두개의 원소가 포함된 집합을 하나로 합침'''
+def find_parent(parent, x):
+    if parent[x] != x:
+        return find_parent(parent, parent[x])
+    return x
+# 개선된 방법 ( 경로 압축 )
+def find_parent_Improved(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v,e = map(int, input().split())
+parent = [0]* (v + 1)
+
+# make itself be parent
+for i in range(1, v + 1):
+    parent[i] = i
+# union연산 수행
+for i in range(e):
+    a, b = map(int, input().split())
+    union_parent(parent, a, b)
+
+print('각 원소가 속한 집합: ', end =' ')
+
+for i in range(1, v + 1):
+    print(find_parent(parent, i), end=' ')
+
+print()
+
+print('parentTable: ', end='')
+for i in range(1, v + 1):
+    print(parent[i], end=' ')
