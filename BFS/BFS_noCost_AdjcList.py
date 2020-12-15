38f4e765fa923b12e0e1b99c03da8bcfa6787031
Caumannerman <uigohiiming2@gmail.com>
2020-12-15 14:06:44 +0900

    Review&Modify BFS_noCost_AdjcList

--- BFS/BFS_noCost_AdjcList.py
+++ BFS/BFS_noCost_AdjcList.py
@@ -4,11 +4,7 @@
    # 2. 큐에서 하나를 꺼내, 해당 노드의 인접 노드 중에서 방문X 노드를 모두 큐에 삽입 + 방문처리
    # 3. 2번을 수행할 수 없을 때까지 반복
 
-
-from collections import deque
-
-graph = [ [], [2,3,8], [1,7], [1,4,5], [3,5], [3,4], [7], [2,6,8], [1,7]]
-
+"""
 visited = [False]*9
 
 queue = deque([])
@@ -28,6 +24,41 @@ def bfs( graph, visited, vertex):
 
 
 bfs(graph, visited, 1)
+"""
+
+from collections import deque
+
+# 노드,간선  갯수
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+
+#간선 입력받기
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+start = int(input())
+visited = [False]*(n+1)
+
+def bfs(graph, start, visited):
+    q = deque([])
+    visited[start] = True
+    q.append(start)
+    print([q[0]])
+    while q:
+        now = q.popleft()
+        temp = []
+        for i in graph[now]:
+            if not visited[i]:
+                q.append(i)
+                visited[i] = True
+                temp.append(i)
+        print(temp)
+
+
+
+bfs(graph, start, visited)
 
 
 
