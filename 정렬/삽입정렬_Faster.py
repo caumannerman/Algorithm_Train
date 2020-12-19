24ccf1c7387a7d54cbbb6f96f0825433f47112f1
Caumannerman <uigohiiming2@gmail.com>
2020-12-19 11:43:55 +0900

    small fix

--- 정렬/삽입정렬_Faster.py
+++ 정렬/삽입정렬_Faster.py
@@ -1,15 +1,13 @@
 data = [9, 3, 5, 6, 2, 1, 8, 4, 7,100,0.3, 1251, 99, 45,32,5412]
 
+
 for i in range(1, len(data)):
     for j in range(0, i):
-        if data[i] > data[j]:
-            continue
-        else:
+        if data[i] < data[j]:
             temp = data[i]
-            for k in range(i-1, j-1, -1):
-                data[k+1] = data[k]
+            for k in range(i, j, -1):
+                data[k] = data[k-1]
             data[j] = temp
             break
-
 
 print(data)
