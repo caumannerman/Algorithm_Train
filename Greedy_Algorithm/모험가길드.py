4f6af9266025926e6c63b5873dfe0164b5a9bf4d
Caumannerman <uigohiiming2@gmail.com>
2021-02-09 16:42:43 +0900

    Revise_Greedy_모험가길드
    효율성 최적화

--- Greedy_Algorithm/모험가길드.py
+++ Greedy_Algorithm/모험가길드.py
@@ -2,46 +2,21 @@
 모험가 길드를 안전하게 구성하고자 공포도가 x인 모험가는 반드시 x명 이상으로 구성한 모험가 그룹에 참여해야 여행을 떠날 수 있다.
 N명의 모험가에 대한 정보가 주어졌을 때, 여행을 떠날  수 있는 그룹수의 최댓값을 구하는 프고그램을 작성하세요'''
 
-N = int(input())
+# 아래의 내가 만든 중, 코드가 가장 빠른 코드
 
-temp = input()
-temp= temp.split()
-for i in range(0,len(temp)):
-    temp[i] = int(temp[i])
+gongpo = list(map(int, input().split()))
 
-temp.sort()
-
-trash = 0
 count = 0
+now = 0
 
+for i in range(len(gongpo)):
+    now += 1
+    if gongpo[i] == now:
+        count += 1
+        now = 0
 
-for i in range(0,len(temp)-1) :
-    if temp[i] == temp[i+1]:
-        continue
-    else:
-        if (i+1-trash)//temp[i]>=1 :
-            count +=  1
-            trash -= temp[i]
-        else:
-            continue
+print(count)
 
-'''
-n = int(input())
-data = list(map(int, input().split()))
-data.sort()
-
-result = 0
-count = 0
-
-for i in data:
-    count += 1
-    if count >=i:
-        result += 1
-        count = 0
-        
-print(result)
-
-'''
 
 
 
