d247f49ac048268aa6d9e3633b9ab45188e7b395
Caumannerman <uigohiiming2@gmail.com>
2021-02-10 19:05:27 +0900

    Revise_Implementation_상하좌우

--- 구현/상하좌우.py
+++ 구현/상하좌우.py
@@ -1,65 +1,34 @@
-
-
-import timeit
-
-
+"""여행가 A는 NxN크기의 정사각형 공간위에 이다. 1,1 위치에서 출발하여 상,하,좌,우 로 이동할 수 있다.
+첫 째 줄에 공간의 한 변의 길이 N이 주어지고, 두번째 줄에 이동계획이 L L R U D와 같이 공백을 기준으로, L,R,U,D로 주어진다.
+1,1에서 왼쪽 혹은 위로 움직이는 것과 같은 불가능한 움직임은 무시된다. 이동계획을 모두 마치고난 후 위치를 공백을 기준으로 출력해라."""
 n = int(input())
-
-plan = list(input().split())  # 그냥 input().spllit() 해도, 공백기준으로 잘라서 리스트가 됨!
-
-
-
-location = [1,1]  # 굳이 1,1 리스트로 안 놓고, 그냥 x,y로 별개의 변수로 두는 것이 더 효율적일 수도 있다.
-
-for i in plan:
-    if i == "R":
-        if location[1] < n:
-            location[1] = location[1]+1
-        else: pass
-    elif i == "L":
-        if location[1]>1:
-            location[1] = location[1]-1
-        else: pass
-    elif i == "U":
-        if location[0]>1:
-            location[0] = location[0]-1
-        else: pass
-    else:
-        if location[0] < n:
-            location[0] = location[0]+1
-        else: pass
-
-print(location[0],location[1])
-
-
-
-stop = timeit.default_timer()
-
-# s(초) 단위로 나온다.
-print(stop - start)
-
-
-
-## 이 문제는 방향이 4군데라서 간단히 엘스이프문으로 나눌 수 있었지만, 종류, 방향이 많아질수록 더 일반화해서 풀어줘야함.
-#
-n = int(input())
+data = list(input().split())
+# 굳이 1,1 리스트로 안 놓고, 그냥 x,y로 별개의 변수로 두는 것이 더 효율적일 수도 있다.
 x, y = 1, 1
-plans = input().split()
 
-dx = [0,0, -1, 1]
-dy = [-1, 1, 0, 0]
-move_types = ['L', 'R', 'U', 'D']
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
-for plan in plans:
+D = ['R', 'L', 'D', 'U']
 
-    for i in range(len(move_types)):
-        if plan == move_types[i]:
-            nx = x + dx[i]
-            ny = y + dy[i]
-    if nx<1 or ny <1 or nx>n or ny>n:
-        continue
+for i in data:
+    for j in range(len(D)):
+        if i == D[j]:
+            if x + dx[j] > 0 and y + dy[j] > 0 and x + dx[j] <= n and y + dy[j] <= n:
+                x += dx[j]
+                y += dy[j]
+                break
 
-    x, y = nx, ny
+print( y,x)
 
 
-print(x, y)
+
+
+
+
+
+
+
+
+
+
