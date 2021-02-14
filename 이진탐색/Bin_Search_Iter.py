07a9553fd644d4475ab75b99e92b05ac2d7560ba
Caumannerman <uigohiiming2@gmail.com>
2021-02-14 16:07:56 +0900

    Binary_Search_Iter_fun

--- 이진탐색/Bin_Search_Iter.py
+++ 이진탐색/Bin_Search_Iter.py
@@ -1,13 +1,10 @@
 
 #이미 정렬된 리스트
-array1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+array1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
 
 # target이 몇 번 째 인덱스에 있는지 그 번호를 return 해준다
-def BS(array, target):
-
-    start = 0
-    end = len(array)-1
-
+def BS_iter(array, target, start, end):
+# while문 안의 마지막 if문을 지우고 , while start <= end로 해주면 더 간결.
     while True:
         mid = (start + end) //2
 
@@ -28,7 +25,8 @@ def BS(array, target):
 
 target = int(input("찾고자 하는 숫자를 입력하세요 : "))
 
-result = BS(array1, target)
+result = BS_iter(array1, target,0,len(array1)-1)
+# 여기 매개변수를 넣어줄 때 end의 위치에 len(array1) -1로 딱 마지막원소 index를 맞춰 넣어줘야함. 아니면, 배열의 모든 원소들보다 큰 target이 들어왔을 때 index out of range뜸.
 if result == None:
     print("해당 target은 존재하지 않습니다!")
 
