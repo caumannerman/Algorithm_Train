b3e5c45e8cef31ff0ffb15480032a655a5106ff4
Caumannerman <uigohiiming2@gmail.com>
2021-10-18 02:48:37 +0900

    acmicpc#4796_캠핑 (Greedy)

--- Greedy_Algorithm/acmicpc#4796_캠핑.py
+++ Greedy_Algorithm/acmicpc#4796_캠핑.py
@@ -8,22 +8,25 @@
 
 캠핑장을 연속하는 P일 중, L일동안만 사용할 수 있다. 강산이는 이제 막 V일짜리 휴가를 시작했다.
 강산이가 캠핑장을 최대 며칠동안 사용할 수 있을까? (1 < L < P < V)"""
+
 # 입력 : 입력은 여러 개의 테스트 케이스로 이루어져 있다. 각 테스트 케이스는 한 줄로 이루어져 있고, L, P, V를 순서대로 포함하고 있다.
 # 모든 입력 정수는 int범위이다. 마지막 줄에는 0이 3개 주어진다.
 # 출력 : 각 테스트 케이스에 대해서, 강산이가 캠핑장을 최대 며칠동안 사용할 수 있는지 예제 출력처럼 출력한다.
 
 import sys
 input = sys.stdin.readline
-case = 0
+
+data = []
 while True:
-    case += 1
-    a, b, c = map(int, input().split())
-    if a ==0 and b== 0 and c == 0:
+    l, p, v = map(int, input().split())
+    if l == 0:
         break
-    result = (c // b) * a
-    rest = c % b
-    if a >= rest:
-        result += rest
-    else:
-        result += a
-    print("Case " + str(case) + ": " + str(result))
+    data.append((l,p,v))
+
+result = 0
+for i in range(len(data)):
+    result = 0
+    l, p, v = data[i]
+    result += (v // p) * l
+    result += min( v% p, l)
+    print("Case {0}: {1}".format(i+1, result))
