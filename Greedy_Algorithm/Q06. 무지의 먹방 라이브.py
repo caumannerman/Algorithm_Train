7b4e3a0e22a520b7b69af3dc10552a9cf673ce48
Caumannerman <uigohiiming2@gmail.com>
2021-12-03 20:50:43 +0900

    .

--- Greedy_Algorithm/Q06. 무지의 먹방 라이브.py
+++ Greedy_Algorithm/Q06. 무지의 먹방 라이브.py
@@ -65,23 +65,26 @@ def solution(food_times, k):
 import heapq
 
 
-def solution(food_times, k):
-    answer = -1
-    h = []
-    for i in range(len(food_times)):
-        heapq.heappush(h, (food_times[i], i + 1))
-    food_num = len(food_times)  # 남은 음식 개수
-    previous = 0  # 이전에 제거한 음식의 food_time
 
-    while h:  # 먹는데 걸리는 시간: (남은 양) * (남은 음식 개수)
-        t = (h[0][0] - previous) * food_num  # 시간이 남을 경우 현재 음식 빼기
-        if k >= t:
-            k -= t
-            previous, _ = heapq.heappop(h)
-            food_num -= 1  # 시간이 부족할 경우(음식을 다 못먹을 경우) 남은 음식 중에 먹어야 할 음식 찾기
+
+
+def solution(food_times, k):
+    q = []
+    for i in range(len(food_times)):
+        heapq.heappop(q, (food_times[i], i + 1))
+
+    rest_food = len(food_times)
+    former_del = 0
+
+    while q:
+
+        if k >= (q[0][0] - former_del) * rest_food:
+            now_time, _ = heapq.heappop(q)
+            k -= (now_time - former_del) * rest_food
+            rest_food -= 1
+            former_del = now_time
         else:
-            idx = k % food_num
-            h.sort(key=lambda x: x[1])
-            answer = h[idx][1]
-            break
-    return answer
+            q.sort(key=lambda x: x[1])
+            idx = k % rest_food
+            return q[idx][1]
+    return -1
