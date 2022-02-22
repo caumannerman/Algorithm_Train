d8569bc6220675568d4c022016fea3df2cb159cf
Caumannerman <uigohiiming2@gmail.com>
2022-02-22 13:57:00 +0900

    Revise #1987_알파벳 (Back Tracking)

--- BackTracking/acmicpc#1987_알파벳.py
+++ BackTracking/acmicpc#1987_알파벳.py
@@ -1,4 +1,5 @@
 import sys
+
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
@@ -6,31 +7,28 @@ data = []
 for _ in range(n):
     data.append(list(input().rstrip()))
 
-visited = [[False] * m for _ in range(n)]
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
 count = 0
-visited[0][0] = True
+
 result = set([data[0][0]])
 
-def dfs(x,y):
-    tick = False
+
+def dfs(x, y, level):
+    global count
+    count = max(count, level)
+
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
         if nx < 0 or ny < 0 or nx >= n or ny >= m:
             continue
-        if not visited[nx][ny] and data[nx][ny] not in result:
-            tick = True
-            visited[nx][ny] = True
+        if data[nx][ny] not in result:
             result.add(data[nx][ny])
-            dfs(nx,ny)
+            dfs(nx, ny, level + 1)
             result.remove(data[nx][ny])
-            visited[nx][ny] = False
-    if not tick:
-        global count
-        count = max(count, len(result))
-dfs(0,0)
-print(count)
 
+
+dfs(0, 0, 1)
+print(count)
