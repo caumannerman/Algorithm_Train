d79f39c5790073c54de09155db014422aedbb052
Caumannerman <uigohiiming2@gmail.com>
2022-05-06 22:28:23 +0900

    2020KAKAO인턴십_보석쇼핑_효율성 통과

--- KAKAO_기출문제/2020KAKAO인턴십_보석쇼핑.py
+++ KAKAO_기출문제/2020KAKAO인턴십_보석쇼핑.py
@@ -3,23 +3,32 @@ gems_dict = {}
 
 def solution(gems):
     gem_set = set(gems)
+    kind = len(gem_set)
+    now_kind = 0
     for i in gem_set:
         gems_dict[i] = 0
 
     end = 0
     last_length = 100001
     result = [-1, -1]
+
     for start in range(len(gems)):
-        while 0 in gems_dict.values() and end < len(gems):
+        while now_kind < kind and end < len(gems):
+            # 새로운게 들어올 때만 now_kind추가
+            if gems_dict[gems[end]] == 0:
+                now_kind += 1
             gems_dict[gems[end]] += 1
             end += 1
         if end == len(gems):
-            if 0 in gems_dict.values():
+            if now_kind < kind:
                 break
 
         if end - start < last_length:
             last_length = end - start
             result[0], result[1] = start + 1, end
+
         gems_dict[gems[start]] -= 1
+        if gems_dict[gems[start]] == 0:
+            now_kind -= 1
 
     return result
